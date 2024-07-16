653bf44a44e86706a8421d1364fa9c9b3e9daf5a
Robert Aron Zimmermann <robertn@weg.net>
2024-07-16 09:35:18 -0300

    Correção de texto de outputs

--- functions.py
+++ functions.py
@@ -4,13 +4,18 @@ import getpass
 
 def select_iaron_developer(user_input: str):
     from Systems.IAron.gemini import run_automation
-    return run_automation(user_input)
+    response = run_automation(user_input)
+    if response != '':
+        subprocess.run(['python', f'{response}/main.py'], check=True)
+        return f'Arquivo criado com sucesso em {response}'
+
 
 def select_iaron_run_code(user_input: str):
     from Systems.IAron.gemini import run_automation
     response = run_automation(f""" Observação: Importar funções os e sys no começo do código\n {user_input}""")
     if response != '':
         subprocess.run(['python', f'{response}/main.py'], check=True)
+        return f'Arquivo criado com sucesso em {response}'
 
 def select_chatbot_ppc(user_input: str):
     try:
@@ -24,9 +29,9 @@ def select_chatbot_ppc(user_input: str):
         if response.status_code == 200:
             return response.text
         else:
-            print(f'Ocorreu o erro: {response.status_code}!')
+            return f'Ocorreu o erro: {response.status_code}!'
     except Exception as e:
-        print(f'Ocorreu o erro {e}!')
+        return f'Ocorreu o erro {e}!'
 
 def select_send_email(user_input: str):
     from Systems.MailSender.main import run_system
