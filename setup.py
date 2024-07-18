26ace5f3513a3722965043ede8412b4b27384c48
Robert Aron Zimmermann <robertn@weg.net>
2024-07-18 10:45:05 -0300

    Atualizando arquivo de setup

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 from cx_Freeze import setup, Executable
 
+import sys
+
+sys.setrecursionlimit(10**6)
+
 build_exe_options = {
     "packages": ["flet", "docx", "progressbar", "requests",
                  "openpyxl", "google.generativeai",
                  "win32com", "PyPDF2"],
-    "excludes": ["unittest", "tkinter"],
+    "excludes": ["unittest", "tkinter", "navigator-updater", "nbclassic", "nbclient", "nbconvert", "nbformat",
+                 "networkx", "nltk", "nose", "notebook", "numba", "numexpr", "numpy", "numpy-base", "numpydoc",
+                 "packaging", "pandas", "pandocfilters", "panel", "param", "paramiko", "parsel", "parso", "partd",
+                 "pathspec", "patsy", "pep8", "pexpect", "pickleshare", "pillow", "pip", "pkginfo", "plotly", "pluggy",
+                 "powershell_shortcut", "poyo", "prometheus_client", "prompt-toolkit", "protego", "protobuf",
+                 "pure_eval", "flask"],
     "include_files": ["assets/", "database/", "languages/", "artificial_intelligence/", "Systems/"]
 }
 
@@ -22,4 +31,4 @@ setup(
     executables=[target],
 )
 
-# python setup.py build
+# python setup.py build --force
