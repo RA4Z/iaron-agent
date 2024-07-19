2ee59571cf8fa53ba0981bbbad8a18ca74bbd2c7
Robert Aron Zimmermann <robertn@weg.net>
2024-07-19 15:55:34 -0300

    Alterando limite de tokens

--- Systems/MailSender/gemini.py
+++ Systems/MailSender/gemini.py
@@ -7,7 +7,7 @@ generation_config = {
     "temperature": 0.9,
     "top_p": 0.95,
     "top_k": 64,
-    "max_output_tokens": 8192,
+    "max_output_tokens": 50000,
     "response_mime_type": "text/plain",
 }
 
